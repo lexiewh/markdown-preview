497f44cb5cd06d68cd6cb0f410a935c438ee647f
lexiewh <lexwhite028@gmail.com>
2021-11-29 15:52:15 -0600

    MP-10 connect to server

--- cypress/integration/initial_test.spec.js
+++ cypress/integration/initial_test.spec.js
@@ -1,21 +1,5 @@
-describe('My first test', () => {
-    it('visits the kitchen sink', () => {
-        cy.visit('https://example.cypress.io')
-    })
-    it('finds the content "type"', () => {
-        cy.visit('https://example.cypress.io')
-        cy.contains('type')
-    })
-    it('clicks the link "type"', () => {
-        cy.visit('https://example.cypress.io')
-        cy.contains('type').click()
-    })
-    it('navigates to a new url when clicking on the link', () => {
-        cy.visit('https://example.cypress.io')
-        cy.contains('type').click()
-        cy.url().should('include', '/commands/actions')
-
-        // get an input, type into it, and then verify
-        cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com')
+describe('The home page', () => {
+    it('successfully loads', () => {
+        cy.visit('/')
     })
 })
