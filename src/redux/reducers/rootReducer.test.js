0471810925d40262d12b221eb07babc275c22ff3
lexiewh <lexwhite028@gmail.com>
2021-11-30 10:57:50 -0600

    MP-11 finish redux testing

--- src/redux/reducers/rootReducer.test.js
+++ src/redux/reducers/rootReducer.test.js
@@ -1,8 +1,33 @@
 import rootReducer from "."
 import expect from 'expect'
+import { updateInput, updateOutput } from '../actions'
+
+const initialState = { "html_output": "", "md_input": "" }
 
 describe('Root reducer', () => {
     it('should return initial state', () => {
-        expect(rootReducer(undefined, {})).toEqual({ "html_output": "", "md_input": "" })
+        expect(rootReducer(undefined, {})).toEqual(initialState)
+    })
+
+    it('should handle MD_INPUT', () => {
+        expect(rootReducer(initialState, updateInput('sample input'))).toEqual({
+            md_input: 'sample input',
+            html_output: ''
+        })
+    })
+
+    it('should handle HTML_OUTPUT', () => {
+        expect(rootReducer(initialState, updateOutput('sample output'))).toEqual({
+            md_input: '',
+            html_output: 'sample output'
+        })
+    })
+
+    it('should overwrite existing value', () => {
+        const existingState = { md_input: '', html_output: 'sample output' }
+        expect(rootReducer(initialState, updateOutput('rewriting the output'))).toEqual({
+            md_input: '',
+            html_output: 'rewriting the output'
+        })
     })
 })
