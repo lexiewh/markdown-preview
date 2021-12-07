20cf61632fd61cf9230ca6c4cf06c51b59bdbc20
lexiewh <lexwhite028@gmail.com>
2021-12-07 14:25:26 -0600

    MP-16 completed. update state from input

--- src/components/playground/Editor.js
+++ src/components/playground/Editor.js
@@ -1,15 +1,26 @@
-import { useState } from 'react';
+import { connect } from 'react-redux';
 import CodeEditor from '@uiw/react-textarea-code-editor'
+import { updateInput } from '../../redux/actions';
 
-function MdEditor() {
-    const [code, setCode] = useState('')
+const mapStateToProps = state => {
+    return {
+        md_input: state.md_input
+    }
+}
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateCode: (event) => dispatch(updateInput(event))
+    }
+}
+
+function MdEditor({ md_input, updateCode }) {
     return(
         <div id='editor' data-cy='editor'>
             <CodeEditor
-                value={code}
+                value={md_input}
                 language="md"
-                onChange={(evn) => setCode(evn.target.value)}
+                onChange={(evn) => updateCode(evn.target.value)}
                 padding={15}
                 style={{
                     fontSize: 12,
@@ -21,4 +32,4 @@ function MdEditor() {
     )
 }
 
-export default MdEditor
+export default connect(mapStateToProps, mapDispatchToProps)(MdEditor)
--- src/components/playground/Preview.js
+++ src/components/playground/Preview.js
@@ -1,9 +1,17 @@
-function Preview() {
+import { connect } from 'react-redux'
+
+const mapStateToProps = state => {
+    return {
+        md_input: state.md_input
+    }
+}
+
+function Preview({ md_input }) {
     return (
         <div id='preview' data-cy='preview'>
-            <p>Preview</p>
+            <p>{md_input}</p>
         </div>
     )
 }
 
-export default Preview
+export default connect(mapStateToProps)(Preview)
